dc4f12db94fd57e514149420bc0399e0da43409e
natmat <natmattison@gmail.com>
2020-05-19 19:16:59 -0400

    adding copy for services section

--- src/components/sections/services.js
+++ src/components/sections/services.js
@@ -7,7 +7,29 @@ const Services = () => (
   <StyledSection>
     <ServicesContainer id="services">
       <ServicesTitle>Services</ServicesTitle>
-      <Subtitle>Coming Soon</Subtitle>
+      <ServicesText>
+        All engagements are entirely custom and designed in partnership with my clients. Generally, these projects may include one or more of the following steps:
+        <h5>Data discovery</h5>
+        What can I learn from the data I have? How can I manage my data more effectively? What analyses may be appropriate for the data I am collecting? What additional pieces of information may be relevant for a particular type of analysis?
+        <h5>Statistical analysis</h5>
+        Are there trends or patterns in my data that I can learn from? Are there different ways to think about a cost / benefit analysis?  What parts of my data are most meaningful?
+        <ul>
+          <li>Regression analysis</li>
+          <li>Time series forecasting</li>
+          <li>Classification or cluster analysis</li>
+          <li>Supervised or unsupervised learning</li>
+        </ul>
+        <h5>Interpretation</h5>
+        What exactly do the statistical results mean for my business? What recommendations can you provide based on these results?
+        <h5>Data visualization</h5>
+        What is the best way to visualize and monitor my data going forward?
+        <ul>
+          <li>Dashboard tool selection</li>
+          <li>Dashboard creation & maintenance</li>
+        </ul>
+        <h5>Stakeholder management</h5>
+        How can I effectively communicate the technical results to key stakeholders in my business?
+      </ServicesText>
     </ServicesContainer>
   </StyledSection>
 )
@@ -29,41 +51,20 @@ const ServicesContainer = styled(Container)`
 
 const ServicesTitle = styled.h3`
   margin: 0 auto 32px;
-  text-align: center;
 `
 
-// const TryItButton = styled.button`
-//   font-weight: 500;
-//   font-size: 14px;
-//   color: white;
-//   letter-spacing: 1px;
-//   height: 60px;
-//   display: block;
-//   margin-left: 8px;
-//   text-transform: uppercase;
-//   cursor: pointer;
-//   white-space: nowrap;
-//   background: ${props => props.theme.color.secondary};
-//   border-radius: 4px;
-//   padding: 0px 40px;
-//   border-width: 0px;
-//   border-style: initial;
-//   border-color: initial;
-//   border-image: initial;
-//   outline: 0px;
-//   &:hover {
-//     box-shadow: rgba(110, 120, 152, 0.22) 0px 2px 10px 0px;
-//   }
-//   @media (max-width: ${props => props.theme.screen.md}) {
-//   }
-//   @media (max-width: ${props => props.theme.screen.sm}) {
-//     margin-left: 0;
-//   }
-// `
-
-const Subtitle = styled.span`
-  ${props => props.theme.font_size.xxsmall}
-  padding-top: 16px;
-  font-size: 14px;
+const ServicesText = styled.span`
   color: ${props => props.theme.color.primary};
+  max-width: 600px;
+  font-family: ${props => props.theme.font.italic};
+  margin-left: 16px;
+  margin-right: 16px;
+
+  h5 {
+    text-align:center;
+    ${props => props.theme.font_size.regular};
+    font-family: ${props => props.theme.font.bold};
+    line-height: 22px;
+    margin: 30px auto 20px;
+  }
 `
