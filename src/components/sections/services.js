b8e709102998f7a3ef67b43e07ba7342eef337b5
natmat <natmattison@gmail.com>
2020-05-20 20:18:43 -0400

    update services formatting

--- src/components/sections/services.js
+++ src/components/sections/services.js
@@ -10,9 +10,10 @@ const Services = () => (
       <ServicesText>
         All engagements are entirely custom and designed in partnership with my clients. Generally, these projects may include one or more of the following steps:
         <h5>Data discovery</h5>
-        What can I learn from the data I have? How can I manage my data more effectively? What analyses may be appropriate for the data I am collecting? What additional pieces of information may be relevant for a particular type of analysis?
+        What can I learn from the data I have?<br/> How can I manage my data more effectively?<br/> What analyses may be appropriate for the data I am collecting?<br/> What additional pieces of information may be relevant for a particular type of analysis?
         <h5>Statistical analysis</h5>
-        Are there trends or patterns in my data that I can learn from? Are there different ways to think about a cost / benefit analysis?  What parts of my data are most meaningful?
+        Are there trends or patterns in my data that I can learn from?<br/>Are there different ways to think about a cost / benefit analysis?<br/> What parts of my data are most meaningful?
+        <br/><br/><i>Specific methodologies may include:</i>
         <ul>
           <li>Regression analysis</li>
           <li>Time series forecasting</li>
@@ -20,9 +21,10 @@ const Services = () => (
           <li>Supervised or unsupervised learning</li>
         </ul>
         <h5>Interpretation</h5>
-        What exactly do the statistical results mean for my business? What recommendations can you provide based on these results?
+        What exactly do the statistical results mean for my business?<br/>What recommendations can you provide based on these results?
         <h5>Data visualization</h5>
         What is the best way to visualize and monitor my data going forward?
+        <br/><br/><i>Specific processes may include:</i>
         <ul>
           <li>Dashboard tool selection</li>
           <li>Dashboard creation & maintenance</li>
